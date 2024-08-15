b64ea044fae993a7b7ce01be1a7da0b20a0d420a
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-08-15 10:57:27 -0300

    ok

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 // const SUPABASE_URL = 'https://pnhxllwvyymnsraxlyrg.supabase.co';
 // const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuaHhsbHd2eXltbnNyYXhseXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxMTc0MzgsImV4cCI6MjAzODY5MzQzOH0.8PiT2TYCQi4NdogJfwID6UcppehD4RxDw43MdpBFdEo';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
-    localStorage: AsyncStorage,
-    detectSessionInUrl: false,
-});
+// export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+//     localStorage: AsyncStorage,
+//     detectSessionInUrl: false,
+// });
